7d933c64e9426fd3dc2f865c33f7705cc68b9f69
Manjit Bedi <dev@manjitbedi.com>
2021-03-22 16:34:18 -0700

    Update PlatformController.cs

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -10,6 +10,8 @@ public class PlatformController : MonoBehaviour
     // speed
     public float speed = 1;
 
+    bool isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +21,40 @@ public class PlatformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // calculate the distance from out target
+
+        HandleInput();
+
+        HandleMovement();
+    }
+
+    private void HandleInput() 
+    {
+        // Check if input axis has been activated
+        if (Input.GetButtonDown("Fire1"))
+        {
+            isMoving = !isMoving;
+        }
+    }
+
+    private void HandleMovement() 
+    {
+        // if we are not moving exit
+        if (!isMoving)
+        {
+            return;
+        }
+
+                // calculate the distance from out target
         float distance = Vector3.Distance(transform.position, target.position);
 
         // have we arrived?
-        if (distance > 0) {
+        if (distance > 0) 
+        {
             // calculate how much we need to move (step) d = v * t
             float step = speed * Time.deltaTime;
 
             // move by the step
             transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-
         }
-
     }
 }
